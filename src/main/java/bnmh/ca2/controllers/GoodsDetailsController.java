433e6afb8737de181645e9fe90467d1369049cf6
Baz333 <bazdagreat333@gmail.com>
2022-12-30 17:33:56 +0000

    Merge master and hash

--- src/main/java/bnmh/ca2/controllers/GoodsDetailsController.java
+++ src/main/java/bnmh/ca2/controllers/GoodsDetailsController.java
@@ -1,6 +1,7 @@
 package bnmh.ca2.controllers;
 
 import bnmh.ca2.models.BakedGood;
+import bnmh.ca2.models.Recipe;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
@@ -10,11 +11,9 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 public class GoodsDetailsController {
 
@@ -27,16 +26,24 @@ public class GoodsDetailsController {
     @FXML
     private ImageView imgView;
     @FXML
-    private String filepath = null;
-    public Button backButton;
-    public Button editButton;
-    public Button fileButton;
-    private ListView<String> recipeList;
+    private ListView<String> recipeList = new ListView<>();
     @FXML
     private Button addRecipeButton;
     @FXML
     private Button backButton;
+
+    public TextField Name;
+    public TextField Origin = null;
+    public TextField Description = null;
+    public Button edit;
+    public Button backEditButton;
+    public ImageView imageView = null;
+    private String filepath = null;
+    public Button editButton;
+    public Button fileButton;
+
     BakedGood bg = MenuController.bg;
+    public static Recipe rec;
 
     public void initialize() {
         nameLabel.setText(bg.getName());
@@ -50,11 +57,107 @@ public class GoodsDetailsController {
             e.printStackTrace();
         }
 
+        repopulate();
+
+        recipeList.setOnMouseClicked(click -> {
+            if (click.getClickCount() == 2) {
+                if (recipeList.getSelectionModel().getSelectedItem() != null) {
+                    String uidSelected = recipeList.getSelectionModel().getSelectedItem();
+                    uidSelected = uidSelected.substring(0, uidSelected.indexOf(":"));
+                    System.out.println(uidSelected);
+                    Recipe temp = bg.getHead();
+                    for(int i = 0; i < Integer.parseInt(uidSelected) - 1; i++) {
+                        temp = temp.getNextRecipe();
+                    }
+                    System.out.println(temp.getName());
+                    rec = temp;
+                    FXMLLoader caseScene = new FXMLLoader(MenuController.class.getResource("recipe-details.fxml"));
+                    try {
+                        recipeList.getScene().setRoot(caseScene.load());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+
+    }
+
+    public void repopulate() {
+        recipeList.getItems().clear();
+        Recipe temp = MenuController.bg.getHead();
+        int i = 1;
+        while (temp != null) {
+            recipeList.getItems().add(i + ": " +
+                    temp.getName() + " (" +
+                    temp.getDesc() + ")");
+            System.out.println(temp.getName() + " (" +
+                    temp.getDesc() + ")");
+            i++;
+            temp = temp.getNextRecipe();
+        }
+    }
+
+    public void OnAddRecipeButton() throws IOException {
+        FXMLLoader addRecipeView = new FXMLLoader(GoodsDetailsController.class.getResource("add-recipe-view.fxml"));
+        addRecipeButton.getScene().setRoot(addRecipeView.load());
+    }
+
+    public void OnFileButton() throws IOException {
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.webp"));
+        File selectedFile = fileChooser.showOpenDialog(fileButton.getScene().getWindow());
+        if(selectedFile != null) {
+            filepath = selectedFile.getAbsolutePath();
+        }
+        InputStream stream = new FileInputStream(filepath);
+        Image image = new Image(stream);
+        imageView.setImage(image);
     }
 
     public void OnBackButton() throws IOException {
         FXMLLoader menuView = new FXMLLoader(AddGoodsController.class.getResource("menu-view.fxml"));
         backButton.getScene().setRoot(menuView.load());
+    }
+
+    public void OnBackEditButton() throws IOException {
+        FXMLLoader menuView = new FXMLLoader(AddGoodsController.class.getResource("baked-good-details.fxml"));
+        backEditButton.getScene().setRoot(menuView.load());
+    }
+
+    public void OnEditButton() throws IOException {
+        FXMLLoader menuView = new FXMLLoader(AddGoodsController.class.getResource("baked-good-details.fxml"));
+        edit.getScene().setRoot(menuView.load());
+        if(!Name.getText().isEmpty()) {
+            bg.setName(Name.getText());
+            nameLabel.setText(Name.getText());
+        }
+        if(!Origin.getText().isEmpty()) {
+            originLabel.setText(Origin.getText());
+            bg.setOrigin(Origin.getText());
+        }
+        if(!Description.getText().isEmpty()){
+            bg.setDesc(Description.getText());
+            descLabel.setText(Description.getText());
+        }
+        if(filepath != null){
+            bg.setFilepath(filepath);
+            imgView = imageView;
+            try {
+                InputStream stream = new FileInputStream(filepath);
+                Image image = new Image(stream);
+                imgView.setImage(image);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void OnEditButtonPressed() throws IOException {
+        FXMLLoader addIngredientView = new FXMLLoader(MenuController.class.getResource("goods-edit.fxml"));
+        editButton.getScene().setRoot(addIngredientView.load());
     }
 
 }
